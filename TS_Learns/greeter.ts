cafd846a0ed6f903952bfcd398abd979efa607c7
Beimo7 <2603343203@qq.com>
2022-12-06 23:18:09 +0800

    TS学习

--- TS_Learns/greeter.ts
+++ TS_Learns/greeter.ts
@@ -1,7 +1,15 @@
-function greeter(person: String) {
-    return 'Hello,' + person;
+let a: number = 90
+let b: string = '90'
+
+// 字面量
+let c: 12
+
+let d: any
+if (typeof d === 'string') {
+  d = '12'
 }
 
-let user = [1, 2, 3]
-
-document.body.innerHTML = greeter()
+let aa: (a: number, b: number) => number
+aa = function(n1, n2):number {
+  return n1 + n2
+}
