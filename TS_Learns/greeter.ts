2b62f82f3a1291c5fb06fd850fe0e9097569ac1b
Beimo7 <2603343203@qq.com>
2022-12-07 18:12:50 +0800

    change

--- TS_Learns/greeter.ts
+++ TS_Learns/greeter.ts
@@ -10,6 +10,42 @@ if (typeof d === 'string') {
 }
 
 let aa: (a: number, b: number) => number
-aa = function(n1, n2):number {
+aa = function (n1, n2): number {
   return n1 + n2
 }
+
+let isDone: boolean = false
+let dicLiteral: number = 6
+let hexLiteral: number = 0xf00d
+let binaryLiteral: number = 0b1010
+let octalLiteral: number = 0o744
+
+let nam: string = 'bob'
+nam = 'smith'
+
+let age: number = 89
+let sentence: string = `hello, my name is ${nam}. i will be ${age + 1} years old next month`
+
+// let list: number[] = [1, 2, 3]
+let list: Array<number> = [1, 2, 3]
+let x: [string, number]
+x = ['bob', 10]
+let notStore: any = 5
+
+function warnUser(): void {
+  console.log('xxx')
+}
+
+let aaa: void = undefined
+let bbb: void = null
+
+let o = {
+  oname: 'atina',
+  oage: 90,
+  osex: 'female'
+}
+
+let { oa, oage} = o
+
+
+
